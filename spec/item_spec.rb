bf875473e23e3f0bed1e2f07aa16fcdf883ecb08
Chris Handley <github.20.alabama3@spamgourmet.com>
2015-02-17 13:46:14 +0000

    item setup with name, base price

---
+++ spec/item_spec.rb
@@ -0,0 +1,13 @@
+require 'item'
+describe Item do
+  it 'can have a price' do
+    item = Item.new
+    item.price = 9.25
+    expect(item.price).to eq 9.25
+  end
+  it 'can have a name' do
+    item = Item.new
+    item.name = 'Lavender Heart'
+    expect(item.name).to eq 'Lavender Heart'
+  end
+end
